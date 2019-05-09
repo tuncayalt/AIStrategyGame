df60e8d39ab358e9c0e633b4b882d17e9b95aca8
tuncay <tuncayalt@gmail.com>
2019-05-09 10:09:00 -0400

    corrected link

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/ai-strategy'));
+app.use(express.static(__dirname + '/dist/AIStrategy'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/ai-strategy/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/AIStrategy/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
